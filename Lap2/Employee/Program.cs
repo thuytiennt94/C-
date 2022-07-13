c56c51cbec283fc73411e262316ecd8a7516cef4
thuytiennt94 <thuytiennt449@gmail.com>
2022-07-13 16:45:24 +0700

    lap2

---
+++ Lap2/Employee/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Employee
+{
+    private int empID;
+    public Employee(int empID)
+    {
+        this.empID = empID;
+    }
+    public override string ToString()
+    {
+        return empID.ToString();
+    }
+}
+class ObjectArray
+{
+    public void Run()
+    {
+        int[] intArray;
+        Employee[] empArray;
+        intArray = new int[5];
+        empArray = new Employee[3];
+
+        //populate the array
+        for (int i = 0; i < empArray.Length; i++)
+        {
+            empArray[i] = new Employee(i + 5);
+        }
+
+        Console.WriteLine("the int array...");
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            Console.WriteLine(intArray[i].ToString());
+        }
+        Console.WriteLine("\nThe employee array.. ");
+        for(int i = 0; i < empArray.Length; i++)
+        {
+            Console.WriteLine(empArray[i].ToString());
+        }
+    }static void Main(string[] args)
+    {
+        ObjectArray arr = new ObjectArray(); ;
+        arr.Run();
+        Console.ReadLine();
+    }
+}
---
+++ Lap2/ForEach/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class ForEach
+{
+    static void Main(string[] args)
+    {
+        DateTime now = DateTime.Now;
+        Random rand = new Random((int)now.Millisecond);
+        int[] Arr = new int[10];
+        for (int x = 0; x < Arr.Length; ++x)
+        {
+            Arr[x] = rand.Next() % 100;
+        }
+        int Total = 0;
+        Console.Write("array values are");
+        foreach (int val in Arr)
+        {
+            Total += val;
+            Console.Write(val + ",  ");
+        }
+        Console.WriteLine("\nAnd the average is {0,0:F1}",
+            (double)Total / (double)Arr.Length);
+        Console.ReadLine();
+    }
+}
